525e45b20a9ee2ddd393fbe5c75550336510654c
racin_david <racin_david@oauh.cz>
2025-04-24 10:33:40 +0200

    Added table

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -4,8 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Genre</title>
+    <!-- Import Tailwind CSS via Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    
+    <table class="table-auto">
+        <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($genres as $genre)
+                <tr>
+                    <td>{{ $genre->name }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
