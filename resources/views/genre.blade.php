817921be52ca71bda3cb4e1ab894e15808dfbf90
davidracin <david.racin06@gmail.com>
2025-05-08 14:57:47 +0200

    Updated genre design

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -7,28 +7,24 @@
         <div class="col-12">
             <h1 class="mb-4">Genres</h1>
             
-            <div class="card">
-                <div class="card-header bg-primary text-white">
-                    <h5 class="card-title mb-0">Genre List</h5>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-striped table-hover">
-                            <thead class="table-dark">
-                                <tr>
-                                    <th>Name</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($genres as $genre)
-                                    <tr>
-                                        <td>{{ $genre->name }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+            <div class="mb-3 p-3 bg-primary text-white rounded">
+                <h5 class="mb-0">Seznam žánrů</h5>
+            </div>
+            <div class="table-responsive shadow rounded">
+                <table class="table table-striped table-hover align-middle mb-0">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Název</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($genres as $genre)
+                            <tr>
+                                <td>{{ $genre->name }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
