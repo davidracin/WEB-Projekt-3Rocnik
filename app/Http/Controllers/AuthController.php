3f8d1fdbe6a9b6a1ee39b9212c1ac5ab1143122e
davidracin <david.racin06@gmail.com>
2025-05-08 15:32:51 +0200

    Czech error message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
         }
 
         return back()->withErrors([
-            'email' => 'Invalid credentials.',
+            'email' => 'Neplatné přihlašovací údaje.',
         ])->onlyInput('email');
     }
 
